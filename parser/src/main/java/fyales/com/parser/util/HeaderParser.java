5f854bef596cf9bcf72cb7d9c92a4d69dd60cce7
fyales <sww201006@126.com>
2015-02-26 21:48:58 +0800

    调整错误

--- parser/src/main/java/fyales/com/parser/util/HeaderParser.java
+++ parser/src/main/java/fyales/com/parser/util/HeaderParser.java
@@ -24,31 +24,34 @@ public class HeaderParser {
         Matcher m = pattern.matcher(s);
 
         Tag tag = new Tag();
-        if (m.group(1) != null && m.group(2) != null){
-            tag.setContent(m.group(2));
-            switch (m.group(1).length()){
-                case H1:
-                    tag.setName(Tag.TAG_H1);
-                    break;
-                case H2:
-                    tag.setName(Tag.TAG_H2);
-                    break;
-                case H3:
-                    tag.setName(Tag.TAG_H3);
-                    break;
-                case H4:
-                    tag.setName(Tag.TAG_H4);
-                    break;
-                case H5:
-                    tag.setName(Tag.TAG_H5);
-                    break;
-                case H6:
-                    tag.setName(Tag.TAG_H6);
-                    break;
-                default:
-                    tag.setName(Tag.TAG_H3);
+        while (m.find()){
+            if (m.group(1) != null && m.group(2) != null){
+                tag.setContent(m.group(2));
+                switch (m.group(1).length()){
+                    case H1:
+                        tag.setName(Tag.TAG_H1);
+                        break;
+                    case H2:
+                        tag.setName(Tag.TAG_H2);
+                        break;
+                    case H3:
+                        tag.setName(Tag.TAG_H3);
+                        break;
+                    case H4:
+                        tag.setName(Tag.TAG_H4);
+                        break;
+                    case H5:
+                        tag.setName(Tag.TAG_H5);
+                        break;
+                    case H6:
+                        tag.setName(Tag.TAG_H6);
+                        break;
+                    default:
+                        tag.setName(Tag.TAG_H3);
+                }
             }
         }
+
         return tag;
     }
 }
