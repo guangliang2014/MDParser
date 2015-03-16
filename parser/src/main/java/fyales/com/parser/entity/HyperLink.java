a5cef9ec7ea9c9a364509cc3b29d34e81efec3d2
fyales <sww201006@126.com>
2015-03-16 17:53:05 +0800

    nothing

--- parser/src/main/java/fyales/com/parser/entity/HyperLink.java
+++
@@ -1,9 +0,0 @@
-package fyales.com.parser.entity;
-
-/**
- * @author fyales
- * @since date 10/02/15
- */
-public class HyperLink extends Tag{
-
-}
--- parser/src/main/java/fyales/com/parser/entity/Image.java
+++
@@ -1,8 +0,0 @@
-package fyales.com.parser.entity;
-
-/**
- * @author fyales
- * @since date 10/02/15
- */
-public class Image {
-}
